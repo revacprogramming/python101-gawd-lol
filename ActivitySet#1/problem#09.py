94bcb29732af2ac5c864e80cb625158db520110a
gawd-lol <94883170+gawd-lol@users.noreply.github.com>
2022-05-23 04:58:39 +0000

    ...

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,19 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fname=input("Enter file name: ")
+fh=open(fname)
+lst=list()
+
+for line in fh:
+
+  for word in line:
+    word=line.rstrip().split()
+    for element in word:
+      if element in line:
+        continue
+      else:
+        lst.append(element)
+
+l=lst.sort()
+print(l)
