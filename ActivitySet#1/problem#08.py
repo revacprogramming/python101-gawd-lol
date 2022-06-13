c4c146fbc4f0688f63c47ca9f797e7a230e85103
gawd-lol <94883170+gawd-lol@users.noreply.github.com>
2022-06-13 04:07:39 +0000

    done

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,4 +1,20 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
-x
+name = input("Enter the file name : ")
+
+file = open(name)
+
+count = 0
+sum = 0
+
+for i in file:
+    if not i.startswith("X-DSPAM-Confidence:"):
+        continue
+    t = i.find("0")
+    num = i[t:]
+    number = float(num)
+    sum=sum + number
+    count = count + 1
+
+avg = sum/count
+print("Average spam confidence:",avg)
--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,4 +1,30 @@
 # Dictionaries
  
-filename = "dataset/mbox-short.txt"
- 
+name = input("Enter the name of the file : ")
+
+file = open(name)
+
+l = []
+
+for line in file:
+    if not line.startswith("From:"): 
+        continue
+
+    line = line.split()
+    l.append(line[1])
+
+count = {}
+
+for word in l:
+    count[word] = count.get(word,0) + 1
+    #get() method returns the value for the specified key if the key is in the dictionary.
+
+maxcount = None
+maxword = None
+
+for word,count in count.items(): 
+    if maxcount == None or count > maxcount:
+        maxcount = count
+        maxword = word
+
+print (maxword,maxcount)
